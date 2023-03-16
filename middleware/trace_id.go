98cf4a3320977e8aad4cce647f2c165d6222af07
pandy <flowphp@gmail.com>
2023-03-16 09:42:52 +0800

    整合公共组件和公共model方法

--- middleware/trace_id.go
+++ middleware/trace_id.go
@@ -6,8 +6,7 @@ import (
 	"github.com/youchuangcd/gopkg/common/utils"
 )
 
-// 中间件(验证参数签名)
-func CheckSign(c *gin.Context) {
+func TraceId(c *gin.Context) {
 	traceId := c.Request.Header.Get(gopkg.RequestHeaderTraceIdKey)
 	if len(traceId) == 0 || len(traceId) > 100 {
 		traceId = utils.GenTraceId(c)
@@ -15,9 +14,5 @@ func CheckSign(c *gin.Context) {
 	c.Set(gopkg.RequestHeaderTraceIdKey, traceId)
 	// Response head
 	c.Header(gopkg.RequestHeaderTraceIdKey, traceId)
-
-	//请求参数
-	// 验证通过，会继续访问下一个中间件
-
 	c.Next()
 }
