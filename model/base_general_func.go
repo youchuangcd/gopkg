713277b55766e879764f6f398f57f6592cb99887
pandy <flowphp@gmail.com>
2023-03-21 17:42:29 +0800

    新增忽略插入并返回受影响的行

--- model/base_general_func.go
+++ model/base_general_func.go
@@ -6,7 +6,6 @@ import (
 	"gorm.io/gorm/clause"
 )
 
-//
 // Add
 // @Description:
 // @param ctx
@@ -14,7 +13,6 @@ import (
 // @param data 必须是指针
 // @param args ...interface{} 支持传递model和批量插入大小参数
 // @return err
-//
 func Add(ctx context.Context, tx *gorm.DB, data interface{}, args ...interface{}) (err error) {
 	db := GetDB(ctx, tx)
 	argLen := len(args)
@@ -32,15 +30,14 @@ func Add(ctx context.Context, tx *gorm.DB, data interface{}, args ...interface{}
 	return
 }
 
-//
 // AddIgnore
-//  @Description: 插入忽略错误
-//  @param ctx
-//  @param tx
-//  @param data
-//  @param args
-//  @return err
 //
+//	@Description: 插入忽略错误
+//	@param ctx
+//	@param tx
+//	@param data
+//	@param args
+//	@return err
 func AddIgnore(ctx context.Context, tx *gorm.DB, data interface{}, args ...interface{}) (err error) {
 	db := GetDB(ctx, tx)
 	argLen := len(args)
@@ -58,7 +55,34 @@ func AddIgnore(ctx context.Context, tx *gorm.DB, data interface{}, args ...inter
 	return
 }
 
+// AddIgnoreAffectedRow
 //
+//	@Description: 忽略插入并返回受影响的行
+//	@param ctx
+//	@param tx
+//	@param data
+//	@param args
+//	@return row
+//	@return err
+func AddIgnoreAffectedRow(ctx context.Context, tx *gorm.DB, data interface{}, args ...interface{}) (row int64, err error) {
+	db := GetDB(ctx, tx)
+	argLen := len(args)
+	if argLen > 0 {
+		if argLen == 1 {
+			db = db.Model(args[0])
+		} else if argLen == 2 {
+			if size, ok := args[1].(int); ok {
+				db = db.Model(args[0])
+				db.CreateBatchSize = size
+			}
+		}
+	}
+	db = db.Clauses(clause.Insert{Modifier: "IGNORE"}).Create(data)
+	row = db.RowsAffected
+	err = db.Error
+	return
+}
+
 // Update
 // @Description:
 // @param ctx
@@ -68,7 +92,6 @@ func AddIgnore(ctx context.Context, tx *gorm.DB, data interface{}, args ...inter
 // @param args
 // @return affectedRow
 // @return err
-//
 func Update(ctx context.Context, tx *gorm.DB, fields []string, data interface{}, args ...interface{}) (affectedRow int64, err error) {
 	db := GetDB(ctx, tx).Select(fields)
 	// 可变参数传入model
@@ -100,7 +123,6 @@ func UpdateSetMapById(ctx context.Context, tx *gorm.DB, model Interface, fields 
 	return
 }
 
-//
 // GetDetailById
 // @Description: 根据id获取详情
 // @param ctx
@@ -109,7 +131,6 @@ func UpdateSetMapById(ctx context.Context, tx *gorm.DB, model Interface, fields 
 // @param res 必须是指针，要不然无法写值
 // @param args
 // @return error
-//
 func GetDetailById(ctx context.Context, model Interface, id uint, res interface{}, args ...interface{}) error {
 	db := GetDB(ctx, nil).Model(&model).Where("id = ?", id)
 	if len(args) == 1 {
@@ -119,14 +140,12 @@ func GetDetailById(ctx context.Context, model Interface, id uint, res interface{
 	return err
 }
 
-//
 // GetDetailByPrimary
 // @Description: 根据主键id获取详情
 // @param ctx
 // @param model 必须是指针，要不然无法写值
 // @param args
 // @return error
-//
 func GetDetailByPrimary(ctx context.Context, model Interface, args ...interface{}) error {
 	db := GetDB(ctx, nil)
 	if len(args) == 1 {
@@ -136,7 +155,6 @@ func GetDetailByPrimary(ctx context.Context, model Interface, args ...interface{
 	return err
 }
 
-//
 // GetDetailByPrimaryInTransaction
 // @Description: 根据主键id获取详情; 在事务中使用
 // @param ctx
@@ -144,7 +162,6 @@ func GetDetailByPrimary(ctx context.Context, model Interface, args ...interface{
 // @param model
 // @param args
 // @return error
-//
 func GetDetailByPrimaryInTransaction(ctx context.Context, tx *gorm.DB, model Interface, args ...interface{}) error {
 	db := GetDB(ctx, tx)
 	if len(args) == 1 {
@@ -154,7 +171,6 @@ func GetDetailByPrimaryInTransaction(ctx context.Context, tx *gorm.DB, model Int
 	return err
 }
 
-//
 // GetPreloadDetailById
 // @Description: 关联获取详情
 // @param ctx
@@ -163,7 +179,6 @@ func GetDetailByPrimaryInTransaction(ctx context.Context, tx *gorm.DB, model Int
 // @param res
 // @param args
 // @return error
-//
 func GetPreloadDetailById(ctx context.Context, model Interface, id uint, res interface{}, args ...interface{}) error {
 	db := GetDB(ctx, nil).Model(&model).Where("id = ?", id)
 	if len(args) > 0 {
@@ -177,7 +192,6 @@ func GetPreloadDetailById(ctx context.Context, model Interface, id uint, res int
 	return err
 }
 
-//
 // ChunkGetDataByCondition
 // @Description: 根据自定义条件分批获取数据
 // @param ctx
@@ -188,7 +202,6 @@ func GetPreloadDetailById(ctx context.Context, model Interface, id uint, res int
 // @param whereStr
 // @param param
 // @return err
-//
 func ChunkGetDataByCondition(ctx context.Context, model Interface, fields []string, res interface{}, limit int, whereStr string, param ...interface{}) (err error) {
 	err = GetDB(ctx, nil).Model(model).Select(fields).Where(whereStr, param...).Limit(limit).Find(res).Error
 	return
