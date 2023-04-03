32dd954b4d492c1e42a57df8cbc4da8db8c1d6ec
pandy <flowphp@gmail.com>
2023-04-03 17:06:20 +0800

    还未完善的rocketmq客户端

--- apache-rocketmq/client.go
+++ apache-rocketmq/client.go
@@ -1,26 +1,51 @@
 package apacherocketmq
 
 import (
+	"context"
 	"github.com/apache/rocketmq-client-go/v2"
-	"github.com/apache/rocketmq-client-go/v2/consumer"
 	"github.com/apache/rocketmq-client-go/v2/primitive"
 	"github.com/apache/rocketmq-client-go/v2/producer"
-	"github.com/panjf2000/ants/v2"
-	"github.com/youchuangcd/gopkg/common/utils"
 )
+
+var (
+	logConf LogConfig
+)
+
+// 公共的日志配置
+type LogConfig struct {
+	Category   string
+	Logger     logger
+	Limit      uint
+	ReplaceStr string
+	Producer   bool // 推送日志开启
+}
+
+// Init
+//
+//	@Description:
+//	@param conf
+func Init(conf LogConfig) {
+	logConf = conf
+}
+
+type logger interface {
+	LogDebug(ctx context.Context, logCategory string, logContent map[string]interface{}, msg string)
+	LogInfo(ctx context.Context, logCategory string, logContent map[string]interface{}, msg string)
+	LogWarn(ctx context.Context, logCategory string, logContent map[string]interface{}, msg string)
+	LogError(ctx context.Context, logCategory string, logContent map[string]interface{}, msg string)
+}
 
 type Config struct {
 	EndPoint   string
 	RetryTimes int // 推送消息重试次数
-	GoPoolSize int // 协程池数量
-	GoPool     *ants.Pool
+	Namespace  string
 }
 
 type RocketMQ struct {
-	producerClient rocketmq.Producer
-	consumerClient rocketmq.PushConsumer
-	endPoint       string
-	goPool         *ants.Pool
+	rocketmq.Producer
+	endPoint   string
+	retryTimes int // 推送消息重试次数
+	namespace  string
 }
 
 // New
@@ -30,32 +55,70 @@ type RocketMQ struct {
 //	@return *RocketMQ
 func New(conf Config) *RocketMQ {
 	mq := &RocketMQ{
-		endPoint: conf.EndPoint,
+		endPoint:   conf.EndPoint,
+		retryTimes: conf.RetryTimes,
+		namespace:  conf.Namespace,
 	}
 	var err error
-	mq.producerClient, err = rocketmq.NewProducer(
+	mq.Producer, err = rocketmq.NewProducer(
 		producer.WithNsResolver(primitive.NewPassthroughResolver([]string{mq.endPoint})),
-		producer.WithRetry(conf.RetryTimes),
+		producer.WithRetry(mq.retryTimes),
+		producer.WithNamespace(mq.namespace),
 	)
 	if err != nil {
-
+		panic("rocketmq producer new failed：" + err.Error())
 	}
-	mq.consumerClient, err = rocketmq.NewPushConsumer(
-		consumer.WithNsResolver(primitive.NewPassthroughResolver([]string{mq.endPoint})),
-		consumer.WithRetry(conf.RetryTimes),
-	)
-
-	mq.goPool = conf.GoPool
-	if mq.goPool == nil {
-		var goPoolSize = 1000
-		if conf.GoPoolSize > 0 {
-			goPoolSize = conf.GoPoolSize
-		}
-		goPool, err := utils.NewPool(goPoolSize)
-		if err != nil {
-			panic("初始化协程池失败")
-		}
-		mq.goPool = goPool
+	err = mq.Producer.Start()
+	if err != nil {
+		panic("rocketmq producer start failed：" + err.Error())
 	}
 	return mq
 }
+
+func (p *RocketMQ) SendSync(ctx context.Context, msgs ...*primitive.Message) (res *primitive.SendResult, err error) {
+	res, err = p.Producer.SendSync(ctx, msgs...)
+	if err != nil {
+		logConf.Logger.LogError(ctx, logConf.Category, map[string]interface{}{
+			"err":  err,
+			"msgs": msgs,
+		}, "send msg failed")
+	} else if logConf.Producer {
+		logConf.Logger.LogInfo(ctx, logConf.Category, map[string]interface{}{
+			"producerMsgId": res.MsgID,
+			"msgs":          msgs,
+			"res":           res,
+		}, "send msg success")
+	}
+	return
+}
+
+func (p *RocketMQ) SendAsync(ctx context.Context, mq func(ctx context.Context, result *primitive.SendResult, err error), msgs ...*primitive.Message) (err error) {
+	err = p.Producer.SendAsync(ctx, func(ctx context.Context, res *primitive.SendResult, err error) {
+		mq(ctx, res, err)
+		if err == nil && logConf.Producer {
+			logConf.Logger.LogInfo(ctx, logConf.Category, map[string]interface{}{
+				"producerMsgId": res.MsgID,
+				"msgs":          msgs,
+				"res":           res,
+			}, "send msg success")
+		}
+	}, msgs...)
+	if err != nil {
+		logConf.Logger.LogError(ctx, logConf.Category, map[string]interface{}{
+			"err":  err,
+			"msgs": msgs,
+		}, "send async msg failed")
+	}
+	return
+}
+
+//
+//func (p *RocketMQ) SendOneWay(ctx context.Context, mq ...*primitive.Message) error {
+//	return p.Producer.SendOneWay(ctx, mq...)
+//}
+//func (p *RocketMQ) Request(ctx context.Context, ttl time.Duration, msg *primitive.Message) (*primitive.Message, error) {
+//	return p.Producer.Request(ctx, ttl, msg)
+//}
+//func (p *RocketMQ) RequestAsync(ctx context.Context, ttl time.Duration, callback func(ctx context.Context, msg *primitive.Message, err error), msg *primitive.Message) error {
+//	return p.Producer.RequestAsync(ctx, ttl, callback, msg)
+//}
--- apache-rocketmq/rocketmq_test.go
+++ apache-rocketmq/rocketmq_test.go
@@ -7,10 +7,11 @@ import (
 	"github.com/apache/rocketmq-client-go/v2/consumer"
 	"github.com/apache/rocketmq-client-go/v2/primitive"
 	"github.com/apache/rocketmq-client-go/v2/producer"
+	"github.com/youchuangcd/gopkg/common/utils"
 	"os"
 	"strconv"
-	"sync"
 	"testing"
+	"time"
 )
 
 var address = []string{"123.60.185.100:8200", "123.60.76.68:8200"}
@@ -30,14 +31,29 @@ func TestMq(t *testing.T) {
 		os.Exit(1)
 	}
 
-	for i := 0; i < 100; i++ {
+	startUnixMilli := time.Now().UnixMilli()
+	fixedStep := 5000
+	// 根据页数算出当前订单偏移量：云货每页10条; 默认从1开始
+	var orderOffset int64 = 0
+	//msgs := make([]*primitive.Message, 0, 10)
+	for i := 0; i < 10; i++ {
+		orderOffset = int64(i + 1)
+
+		//orderId := strconv.Itoa(i % 10)
+		// 相同orderId的消息需要保证顺序，不同orderId的消息不需要保证顺序，所以将orderId作为选择队列的sharding key。
+		//msg.WithShardingKey(orderId)
+		randFloat := utils.RandInt(1, 5)
+		//randFloat := 0
+		// 计算定时时间 = 当前时间 + 订单数偏移量 + 随机浮动
+		deliverTime := startUnixMilli + orderOffset*int64(fixedStep) + int64(randFloat)*1000
+
+		deliverTimeStr := strconv.FormatInt(deliverTime, 10)
 		msg := &primitive.Message{
 			Topic: topicName,
-			Body:  []byte("Hello RocketMQ Go Client! " + strconv.Itoa(i)),
+			Body:  []byte(fmt.Sprintf("Hello RocketMQ Go Client! %d; time = %s", i, time.UnixMilli(deliverTime).Format("2006-01-02 15:04:05.999"))),
 		}
-		orderId := strconv.Itoa(i % 10)
-		// 相同orderId的消息需要保证顺序，不同orderId的消息不需要保证顺序，所以将orderId作为选择队列的sharding key。
-		msg.WithShardingKey(orderId)
+		msg.WithProperty("__STARTDELIVERTIME", deliverTimeStr)
+		//msgs = append(msgs, msg)
 		res, err := p.SendSync(context.Background(), msg)
 
 		if err != nil {
@@ -46,6 +62,12 @@ func TestMq(t *testing.T) {
 			fmt.Printf("send message success: result=%s\n", res.String())
 		}
 	}
+	//res, err := p.SendSync(context.Background(), msgs...)
+	//if err != nil {
+	//	fmt.Printf("send message error: %s\n", err)
+	//} else {
+	//	fmt.Printf("send message success: result=%s\n", res.String())
+	//}
 	defer func() {
 		err = p.Shutdown()
 		if err != nil {
@@ -64,18 +86,20 @@ func TestConsumer(t *testing.T) {
 		consumer.WithConsumeFromWhere(consumer.ConsumeFromFirstOffset),
 		consumer.WithNamespace("dev"),
 	)
-	tmp := sync.Map{}
+	//tmp := sync.Map{}
 	err := c.Subscribe(topicName, consumer.MessageSelector{}, func(ctx context.Context, msgs ...*primitive.MessageExt) (consumer.ConsumeResult, error) {
 		//orderlyCtx, _ := primitive.GetOrderlyCtx(ctx)
+		now := time.Now()
 		for _, msg := range msgs {
 			content := string(msg.Body)
-			if _, ok := tmp.Load(content); ok {
-				fmt.Println("重复的消息：", msg.Body)
-			}
-			tmp.Store(content, "存在")
+			fmt.Println("当前时间：", now.Format("2006-01-02 15:04:05.999"), "消息的tag：", msg.GetTags(), "消息内容：", content)
+			//if _, ok := tmp.Load(content); ok {
+			//	fmt.Println("重复的消息：", msg.Body)
+			//}
+			//tmp.Store(content, "存在")
 		}
 		//fmt.Printf("orderly context: %v\n", orderlyCtx)
-		fmt.Printf("subscribe orderly callback: %v \n", msgs)
+		//fmt.Printf("subscribe orderly callback: %v \n", msgs)
 		return consumer.ConsumeSuccess, nil
 	})
 	if err != nil {
