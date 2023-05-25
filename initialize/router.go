bdacf661c9a0994fa41a0ee3e1dc95028db30a6e
pandy <ruoshu1@qq.com>
2023-05-25 15:59:25 +0800

    整合opentelemetry

--- initialize/router.go
+++ initialize/router.go
@@ -8,13 +8,14 @@ import (
 )
 
 func Router(r *gin.Engine, serviceName string) {
+	r.Use(middleware.Recover)
+	//注册中间件
+	r.Use(middleware.TraceId)
+
 	r.Use(otelgin.Middleware(serviceName, otelgin.WithFilter(func(r *http.Request) bool {
 		if r.RequestURI == "/app/healthCheck" {
 			return false
 		}
 		return true
 	})))
-	r.Use(middleware.Recover)
-	//注册中间件
-	r.Use(middleware.TraceId)
 }
