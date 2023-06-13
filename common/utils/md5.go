c6ea45b91f12af19fd26db1cfe5580e45fec6105
pandy <ruoshu1@qq.com>
2023-06-13 10:08:10 +0800

    优化md5写法

--- common/utils/md5.go
+++ common/utils/md5.go
@@ -2,13 +2,21 @@ package utils
 
 import (
 	"crypto/md5"
-	"encoding/hex"
 )
 
+const hextable = "0123456789abcdef"
+
 func MD5V(str []byte) string {
-	//h := md5.New()
-	//h.Write(str)
-	//return hex.EncodeToString(h.Sum(b))
-	sum := md5.Sum(str)
-	return hex.EncodeToString(sum[:])
+	//sum := md5.Sum(str)
+	//return hex.EncodeToString(sum[:])
+
+	src := md5.Sum(str)
+	var dst = make([]byte, 32)
+	j := 0
+	for _, v := range src {
+		dst[j] = hextable[v>>4]
+		dst[j+1] = hextable[v&0x0f]
+		j += 2
+	}
+	return string(dst)
 }
